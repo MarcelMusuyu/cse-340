e33e72127d79d96a3ce5ca41ffc263cb86e212cf
Marcel Nyembo <marcelmusuyu9@gmail.com>
2025-03-21 11:42:23 +0000

    Implementing Inventory view and handling errors

--- server.js
+++ server.js
@@ -30,9 +30,18 @@ app.set("layout", "./layouts/layout") // not at views root
 
 // Index Route
 app.use(static)
-app.use('/inv', inventoryRoute); // Correct usage: mount the router at '/inv'
+app.use('/inv', inventoryRoute) // Correct usage: mount the router at '/inv'
+
+
 
 app.get("/", utilities.handleErrors(baseController.buildHome))
+
+
+
+// Error-generating route
+app.get('/generate-error', (req, res, next) => {
+  next({status: 500, message: 'Intentional server error'})
+});
 
 // File Not Found Route - must be last route in list
 app.use(async (req, res, next) => {
@@ -46,16 +55,41 @@ app.use(async (req, res, next) => {
 * Express Error Handler
 * Place after all other middleware
 *************************/
+
 app.use(async (err, req, res, next) => {
   let nav = await utilities.getNav()
+   let status = null
   console.error(`Error at: "${req.originalUrl}": ${err.message}`)
-  if(err.status == 404){ message = err.message} else {message = 'Oh no! There was a crash. Maybe try a different route?'}
+  if(err.status == 500){ 
+    message = err.message
+    status = err.status
+   
+
+  } else {message = 'Oh no! There was a crash. This is an internal server error?'}
   res.render("errors/error", {
     title: err.status || 'Server Error',
     message,
-    nav
+    nav,
+    status
   })
 })
+
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  let status= null
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  if(err.status == 404){ message = err.message
+    status = err.status
+  } else {message = 'Oh no! There was a crash. Maybe try a different route?'}
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message,
+    nav,
+    status
+  })
+})
+
+
 
 /* ***********************
  * Local Server Information
