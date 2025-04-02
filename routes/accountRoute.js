b96cf177554fdedf60ae734db30b2996d1e11401
Marcel Nyembo <marcelmusuyu9@gmail.com>
2025-04-02 11:26:22 +0000

    Complition of add-classification and inventory

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -2,6 +2,7 @@
 const express = require("express")
 const router = new express.Router() 
 const accountController = require("../controllers/accountController")
+
 const utilities = require("../utilities/")
 
 const regValidate = require('../utilities/account-validation')
@@ -14,7 +15,7 @@ router.get("/login", utilities.handleErrors(accountController.buildLogin) )
 router.get("/register", utilities.handleErrors(accountController.buildRegister))
 
 router.post('/register',regValidate.registationRules(),regValidate.checkRegData, utilities.handleErrors(accountController.registerAccount))
-router.post('/login', utilities.handleErrors(accountController.loginAccount))
+router.post('/login',regValidate.loginRules(), regValidate.checkLogData, utilities.handleErrors(accountController.loginToAccount))
 
 
 module.exports = router;
